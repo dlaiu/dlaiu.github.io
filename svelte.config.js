04877033379c8ef943a477adaf91104ce2626c45
dlaiu <darryllaiu95@gmail.com>
2024-01-06 03:28:03 -0500

    changed layout.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,42 +1,42 @@
-// // import adapter from '@sveltejs/adapter-auto';
-// import adapter from '@sveltejs/adapter-static';
-// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
-
-// /** @type {import('@sveltejs/kit').Config} */
-// const config = {
-// 	kit: {
-// 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-// 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-// 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-// 		adapter: adapter()
-// 	},
-// 	preprocess: vitePreprocess(),
-
-// 	paths: {
-
-// 		base: process.env.NODE_ENV === 'production' ? '/dlaiu.github.io' : '',
-
-// 	}
-
-
-// };
-
-// export default config;
-
-
+// import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
+	},
+	preprocess: vitePreprocess(),
+
+	paths: {
+
+		base: process.env.NODE_ENV === 'production' ? '/dlaiu.github.io' : '',
+
 	}
+
+
 };
 
 export default config;
+
+
+// import adapter from '@sveltejs/adapter-static';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		adapter: adapter({
+// 			fallback: '404.html'
+// 		}),
+// 		paths: {
+// 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+// 		}
+// 	}
+// };
+
+// export default config;
