8a6afc630aeaab631ebc3f99c8c27f769c4061af
dlaiu <darryllaiu95@gmail.com>
2024-11-26 00:42:14 -0500

    updated layout and info

--- src/lib/card.svelte
+++ src/lib/card.svelte
@@ -3,14 +3,25 @@
     export let summary = "summary"
     export let img_path = "endemic-thumb-2.png"
     export let link = "#"
-    export let width = "full"
+    // export let width = "full"
+    export let tools = []; // Array of tools used
 </script>
 
 <a href={link} target="_blank" class="card flex-shrink-0 md:w-1/4 bg-white shadow-lg rounded-lg overflow-hidden block hover:bg-gray-100 transition duration-300">
-    <img class="w-full rounded-lg max-h-85" src={img_path} alt="thumbnail for project done">
+    <img class="w-full max-h-85" src={img_path} alt="thumbnail for project done">
     <div class="p-4">
         <h2 class="text-xl font-semibold mb-2">{title}</h2>
         <p class="text-gray-700 text-sm mb-2">{summary}</p>
+         <!-- Tools section -->
+         {#if tools.length > 0}
+         <div class="flex flex-wrap gap-2 mt-4">
+             {#each tools as tool}
+                 <span class="text-xs bg-gray-200 text-gray-700 px-2 py-1 rounded-full shadow-sm">
+                     {tool}
+                 </span>
+             {/each}
+         </div>
+     {/if}
     </div>
 </a>
 
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -15,8 +15,8 @@
             <div class="items-center md:text-md mx-14 md:ml-5 mt-3 md:mt-0 xl:text-lg">
                 <p>My name is <a href="https://www.linkedin.com/in/darryl-laiu/" target="_blank" class="underline hover:bg-blue-500 cursor-pointer transition duration-300">Darryl Laiu.</a></p>
                 <p>I'm a multimedia journalist.</p>
-                <p>I'm currently studying <a href="https://journalism.columbia.edu/ms-data-journalism" target="_blank" class="underline hover:bg-blue-300 cursor-pointer transition duration-300">Data Journalism at Columbia Journalism School.</a></p>
-                <p>I used to make videos for <a href="https://mothership.sg" target="_blank" class="underline hover:bg-orange-400 cursor-pointer transition duration-300">Mothership.sg.</a></p>
+                <p>I graduated from the <a href="https://journalism.columbia.edu/ms-data-journalism" target="_blank" class="underline hover:bg-blue-300 cursor-pointer transition duration-300">Data Journalism Program at Columbia Journalism School.</a></p>
+                <p>I currently make videos for <a href="https://bbc.com" target="_blank" class="underline hover:bg-white cursor-pointer transition duration-300">BBC.com.</a></p>
             </div>
         </div>
         <div class="contacts grid grid-flow-col auto-cols-max gap-4 justify-center m-5 md:justify-start md:m-0 md:ml-5">
@@ -37,8 +37,11 @@
                     <path d="M254.896 184.158C252.81 183.926 250.733 183.645 248.671 183.315C250.773 183.574 252.849 183.855 254.896 184.158Z" fill="none" stroke-width="1.5" stroke="currentColor"/>
                     <path d="M180 141.964C163.699 110.262 119.308 51.1817 78.0347 22.044C38.4971 -5.86834 23.414 -1.03207 13.526 3.43594C2.08093 8.60755 0 26.1785 0 36.5164C0 46.8542 5.66748 121.272 9.36416 133.694C21.5786 174.738 65.0603 188.607 105.104 184.156C107.151 183.852 109.227 183.572 111.329 183.312C109.267 183.642 107.19 183.924 105.104 184.156C46.4204 192.847 -5.69621 214.233 62.6582 290.33C137.848 368.18 165.705 273.637 180 225.702C194.295 273.637 210.76 364.771 295.995 290.33C360 225.702 313.58 192.85 254.896 184.158C252.81 183.926 250.733 183.645 248.671 183.315C250.773 183.574 252.849 183.855 254.896 184.158C294.94 188.61 338.421 174.74 350.636 133.697C354.333 121.275 360 46.8568 360 36.519C360 26.1811 357.919 8.61012 346.474 3.43851C336.586 -1.02949 321.503 -5.86576 281.965 22.0466C240.692 51.1843 196.301 110.262 180 141.964Z" fill="black"/>
                     </svg>
-                    
             </a>
+            <h1 class="text-3xl">|</h1>
+            <a href="https://github.com/dlaiu" target="_blank">
+                <img src="github.png" alt="" class="size-9">
+            </a> 
         </div>
         
         <div id="navigation-arrow" class="self-center absolute bottom-0 mb-12 animate-bounce">
@@ -55,20 +58,44 @@
     
     <section id="projects" class="container mx-auto flex flex-col min-h-screen snap-always snap-center">
         <h1 class="text-5xl font-semibold pt-6 mx-8 md:pb-8 md:text-7xl"> >Projects</h1>
+
+        <div class="flex flex-col md:flex-row md:text-md mx-14 md:ml-10 mt-3 md:mt-0 md:mb-10 xl:text-lg">
+            <blockquote class="border-l-4 border-orange-400 pl-4 text-lg md:text-xl leading-relaxed">
+                <p>
+                    <b>Some highlights:</b> I did a crash course in computational linguistics to 
+                    <a href="https://dlaiu.github.io/tiktok-accent/" target="_blank" class="underline hover:bg-orange-400 cursor-pointer transition duration-300">analyse the "influencer accent"</a>, 
+                    <a href="http://localhost:5173/data-stories/ign-game-reviews" target="_blank" class="underline hover:bg-orange-400 cursor-pointer transition duration-300">scraped 13,000 reviews from IGN</a> to find out if they really give an overwhelming number of 7s, and 
+                    <a href="https://github.com/dlaiu/bigfoot" target="_blank" class="underline hover:bg-orange-400 cursor-pointer transition duration-300">manually read and categorised</a> 4,000 reports of Bigfoot sightings to find out what he looks like.
+                    In a previous and current life as a video and multimedia journalist, I make explainers on topics like 
+                    <a href="https://www.bbc.com/reel/video/p0k4y1h6/who-should-pick-up-the-bill-for-climate-damage-" target="_blank" class="underline hover:bg-orange-400 cursor-pointer transition duration-300">climate finance</a>, 
+                    <a href="https://www.bbc.com/reel/video/p0jyrzsg/the-history-of-swing-states-in-the-us" target="_blank" class="underline hover:bg-orange-400 cursor-pointer transition duration-300">the history of swing states in the US </a>, 
+                    and <a href="https://www.youtube.com/watch?v=zKVdTcY9Yxw" target="_blank" class="underline hover:bg-orange-400 cursor-pointer transition duration-300">why TikTok is so addictive.</a>
+                </p>
+            </blockquote>
+        </div>
+        
     
-        <h2 class="text-3xl my-4 md:my-0 md:mb-4 mx-10 md:mx-24"> ~ Data Stories</h2>
+        <h2 class="text-3xl my-4 md:my-0 md:mb-4 mx-10 md:mx-12"> ~ Data Stories</h2>
     
-        <div class="data-stories flex flex-col md:flex-row md:flex-nowrap gap-5 mx-16 md:mx-24 overflow-x-scroll">
+        <div class="data-stories flex flex-col md:flex-row md:flex-wrap gap-5 mx-10 md:mx-0 md:pl-20">
 
-            <Card width="1/4" title="What do we know about bigfoot?" img_path="bigfoot-banner.webp" summary="From more than 4,000 reports." link="https://dlaiu.github.io/bigfoot/" />
+            <Card title="Why do TikTokers talk differently?" img_path="tiktok-thumb.png" summary="I scraped and analysed more than 300 videos from TikTok to find the pattern." link="https://dlaiu.github.io/tiktok-accent/" tools={['TikTok Scraping', 'Audio analysis (Praat)', 'Svelte', 'Scrollytelling', 'D3.js', 'Huggingface', 'Pandas']}/>
 
-            <Card width="1/4" title="The value of a game review" img_path="banner-photo.png" summary="A closer look at IGN's 7s." link="data-stories/ign-game-reviews" />
+            <Card width="1/4" title="What do we know about bigfoot?" img_path="bigfoot-banner.webp" summary="I went through more than 4,000 reports of bigfoot sightings to learn more." link="https://dlaiu.github.io/bigfoot/" tools={['scrollytelling', 'Datawrapper', 'Qualitative Analysis']} />
 
-            <Card width="1/4" title="When was the best SNL era, objectively" img_path="snl-cast-thumb.png" summary="A look at the numbers." link="https://dlaiu.github.io/snl-dream-team" />
+            <Card width="1/4" title="The value of a game review" img_path="banner-photo.png" summary="IGN is famous for giving 7s in their reviews. But is it actually true? I scraped about 13,000 of their reviews to find out." link="data-stories/ign-game-reviews" tools={['BeautifulSoup', 'Pandas', 'ggplot', 'Secret APIs', 'ai2html', 'Illustrator']}/>
 
-            <Card width="1/4" title="Why do all malls in Singapore feel the same?" img_path="mall-thumb.png" summary="A data exploration into the 'generic-ness' of Singapore malls" link="https://dlaiu.github.io/generic-malls-singapore/" />
+            <Card title="How John Oliver does good journalism while making you laugh" img_path="john-oliver-thumb.png" summary="I broke down an episode of Last Week Tonight with John Oliver to argue why he actually is doing good journalism." link="https://dlaiu.github.io/john-oliver-narrative/" tools={['d3.js', 'Svelte', 'ai2html', 'Svelte Slides']}/>
 
-            <Card width="1/4" title="Here's how much your neighbours are selling their flat for" img_path="highest-hdb-price.png" summary="An interactive summary of where some of the most expensive HDBs are in Singapore." link="https://dlaiu.github.io/latest-highest-hdb-resale-price/" />
+            <Card width="1/4" title="When was the best SNL era, objectively" img_path="snl-cast-thumb.png" summary="People say that SNL hasn't been good since [whenever you grew up]. I came up with a metric to end the debate once and for all." link="https://dlaiu.github.io/snl-dream-team" tools={['Playwright','BeautifulSoup', 'Pandas', 'ggplot']}/>
+
+            <Card width="1/4" title="Why do all malls in Singapore feel the same?" img_path="mall-thumb.png" summary="If you've lived in Singapore, you know how important malls are every day life. But do they all have to feel the same? Why??" link="https://dlaiu.github.io/generic-malls-singapore/" tools={['Playwright', 'BeautifulSoup', 'fuzzywuzzy', 'Pandas', 'Datawrapper']}/>
+
+            <Card title="Here's where every WIFI hotspot is in NYC" img_path="wifi-thumb.png" summary="Visualising every NYC Wifi in the city and counting them in each neighborhood." link="https://dlaiu.github.io/nyc-wifi-neighborhoods/" tools={['Mapbox', 'NYC Open Data', "Javascript", "HTML", "CSS"]}/>
+
+            <Card width="1/4" title="Here's how much your neighbours are selling their flat for" img_path="highest-hdb-price.png" summary="My first project! Public housing is the only thing Singaporeans my age ever talk about. Here's a handy tool to check how much they're being sold for now." link="https://dlaiu.github.io/latest-highest-hdb-resale-price/" tools={['APIs', 'geocoding', 'Mapbox']}/>
+
+            
         </div>
         
         <div class="flex flex-row md:mx-24">
@@ -86,19 +113,22 @@
               
         </div>
     
-        <div class="videos flex flex-col md:flex-row md:flex-nowrap gap-5 mx-16 md:mx-24 overflow-x-scroll">
-            <Card width="1/4" title="Why the Russia-Ukraine war isn't ending soon" img_path="ukraine-thumb.png" summary="Is the war even near a conclusion? How do wars even end? We speak to an expert to find out." link="https://www.youtube.com/watch?v=MMdG73HUYpU&list=PLpFwA1oP_ZZBPEay4Dybken9Rz6owzhd3&index=3"/>
-            <Card width="1/4" title="Straight to the Point with George Yeo" img_path="george-yeo-thumb.jpg" summary="I sat down with the former Minister for Foreign Affairs to talk about the Singaporean Identity, SAP schools, and his role in CECA." link="https://www.youtube.com/watch?v=UibCFxEfAwQ&list=PLpFwA1oP_ZZBPEay4Dybken9Rz6owzhd3&index=6"/>
-            <Card width="1/4" title="Why is TikTok so addictive?" img_path="WIITY1.jpg" summary="STOP. Are you scrolling on social media right now and can't stop? Well, it's not entirely your fault." link="https://www.youtube.com/watch?v=zKVdTcY9Yxw"/>
-            <Card width="1/4" title="How does one become the Prime Minister of Singapore?" img_path="PM_thumb.png" summary="We look at how past Prime Ministers were chosen." link="https://www.youtube.com/watch?v=4ii5AILzsEw"/>
-            <Card width="1/4" title="Is Singapore becoming too expensive to live in?" img_path="inflation-thumb.jpg" summary="You've probably noticed by now that almost everything seems to be getting more expensive lately. Why?" link="https://www.youtube.com/watch?v=4ii5AILzsEw"/>
-            <Card width="1/4" title="Why do Muslim women choose to wear the tudung?" img_path="muis-tudung-thumb-3.png" summary="We talk to 3 Muslim people about why they choose to wear the headscarf." link="https://www.youtube.com/watch?v=l_MGGk1-ab4"/>
-            <Card width="1/4" title="Why Singapore is not ready for an endemic Covid-19" img_path="endemic-thumb-2.png" summary="At the cusp of opening up, Singaporeans were still arguing that we shouldn't. Why?" link="https://www.youtube.com/watch?v=AC709_eYJmM" />
+        <div class="videos flex flex-col md:flex-row md:flex-wrap gap-5 mx-10 md:mx-0 md:pl-20">
+            <Card title="Who should pick up the bill for climate damage?" img_path="climate-finance-yt-thumb.jpg" summary="Many developing countries argue that the world's biggest polluters, and those that have benefited economically from it, have not paid their fair share of climate finance to date.
+
+But what is even considered fair? " link="https://www.bbc.com/reel/video/p0k4y1h6/who-should-pick-up-the-bill-for-climate-damage-" tools={['For BBC', "Final Cut Pro", "DataWrapper", "Illustrator"]}/>
+            <Card width="1/4" title="Why the Russia-Ukraine war isn't ending soon" img_path="ukraine-thumb.png" summary="Is the war even near a conclusion? How do wars even end? We speak to an expert to find out." link="https://www.youtube.com/watch?v=MMdG73HUYpU&list=PLpFwA1oP_ZZBPEay4Dybken9Rz6owzhd3&index=3" tools={['For Mothership', "Premiere Pro", "Photoshop", "Filming", "Scripting"]}/>
+            <Card width="1/4" title="Straight to the Point with George Yeo" img_path="george-yeo-thumb.jpg" summary="I sat down with the former Minister for Foreign Affairs to talk about the Singaporean Identity, SAP schools, and his role in CECA." link="https://www.youtube.com/watch?v=UibCFxEfAwQ&list=PLpFwA1oP_ZZBPEay4Dybken9Rz6owzhd3&index=6" tools={['For Mothership', "Presenting"]}/>
+            <Card width="1/4" title="Why is TikTok so addictive?" img_path="WIITY1.jpg" summary="STOP. Are you scrolling on social media right now and can't stop? Well, it's not entirely your fault." link="https://www.youtube.com/watch?v=zKVdTcY9Yxw" tools={['For Mothership', "Presenting"]}/>
+            <Card width="1/4" title="How does one become the Prime Minister of Singapore?" img_path="PM_thumb.png" summary="We look at how past Prime Ministers were chosen." link="https://www.youtube.com/watch?v=4ii5AILzsEw" tools={['For Mothership', "Scripting", "Filming", "Presenting"]}/>
+            <Card width="1/4" title="Is Singapore becoming too expensive to live in?" img_path="inflation-thumb.jpg" summary="You've probably noticed by now that almost everything seems to be getting more expensive lately. Why?" link="https://www.youtube.com/watch?v=4ii5AILzsEw" tools={['For Mothership', "Scripting", "Filming"]}/>
+            <Card width="1/4" title="Why do Muslim women choose to wear the tudung?" img_path="muis-tudung-thumb-3.png" summary="We talk to 3 Muslim people about why they choose to wear the headscarf." link="https://www.youtube.com/watch?v=l_MGGk1-ab4" tools={['For Mothership', "Premiere Pro", "Photoshop", "Scripting", "Filming"]}/>
+            <Card width="1/4" title="Why Singapore is not ready for an endemic Covid-19" img_path="endemic-thumb-2.png" summary="At the cusp of opening up, Singaporeans were still arguing that we shouldn't. Why?" link="https://www.youtube.com/watch?v=AC709_eYJmM" tools={['For Mothership', "Scripting", "Presenting"]}/>
         </div>
     
-        <h2 class="text-3xl pt-6 pb-4 mx-10 md:mx-24"> ~ Articles</h2>
+        <h2 class="text-3xl pt-6 pb-4 mx-10 md:mx-24"> ~ Writing</h2>
     
-        <div class="articles flex flex-col md:flex-row md:flex-nowrap gap-5 mx-16 md:mx-24 md:overflow-x-scroll">
+        <div class="articles flex flex-col md:flex-row md:flex-wrap gap-5 md:mx-0 md:pl-20">
             <Card title="3 first time S'pore Olympians on why Tokyo Games might also be their last" img_path="olympians-cover.png" summary="Singapore's Olympians, unfiltered." link="https://mothership.sg/2021/08/singapore-first-time-olympians-retiring/"/>
             <Card title="Cancelling & cancel culture: Are they relevant to us in S’pore & why should we care?" img_path="cancel-culture-explained-2.png" summary="Cancelling and cancel culture have been hot topics of conversation recently. We look into their history, criticisms, and why understanding them concerns us." link="https://mothership.sg/2020/08/cancel-culture-explainer/" />
             <Card title="Minimum wage vs Progressive Wage Model: What's the difference?" img_path="minwage-cover.png" summary="Amidst all the debate online, we look at the key differences between the Progressive Wage Model and the minimum wage." link="https://mothership.sg/2020/07/minimum-wage-progressive-wage-model-differences/"/>
